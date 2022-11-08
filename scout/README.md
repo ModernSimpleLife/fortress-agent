0b3002c741264a3d356fb3a196f8ab51b9793aeb
Lukas Herman <lukasman@amazon.com>
2022-11-08 18:48:30 -0500

    update with web server

--- scout/README.md
+++ scout/README.md
@@ -3,3 +3,9 @@
 ```
 apt install llvm-dev libclang-dev clang
 ```
+
+## How to flash?
+
+```
+cargo espflash --release --target riscv32imc-esp-espidf --monitor /dev/ttyUSB
+```
--- scout/src/main.rs
+++ scout/src/main.rs
@@ -1,9 +1,25 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-fn main() {
+use tiny_http::{Response, Server};
+
+fn main() -> anyhow::Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
 
-    println!("Hello, world!");
+    let server = Server::http("0.0.0.0:8000").unwrap();
+
+    for request in server.incoming_requests() {
+        println!(
+            "received request! method: {:?}, url: {:?}, headers: {:?}",
+            request.method(),
+            request.url(),
+            request.headers()
+        );
+
+        let response = Response::from_string("hello world");
+        request.respond(response);
+    }
+
+    Ok(())
 }
